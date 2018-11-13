362a4194ea532223ebbaedc2c97e5697e715f38b
Melroy Dsouza <melroydsouza176@gmail.com>
2018-11-13 09:22:18 -0800

    The main driver file for the BinaryTree class
    The main driver class tests all the binary tree class states and methods (To be done in future).

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "BinaryTree.h"
+
+using namespace std;
+
+//to be tested after implementing traversal method.
+void testBinaryTreeAdd() {
+	BinaryTree r1;
+	r1.add(3);
+	r1.add(3);
+	r1.add(4);
+	r1.add(7);
+}
+
+int main() {
+	
+
+
+	cin.get(); //stop cmd from exiting after running code.
+	return 0;
+}
